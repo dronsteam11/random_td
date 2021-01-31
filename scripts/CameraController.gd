d8e22e7153a6d9173e546be89f7910dbbf0ea9f8
dronsteam11 <dronsteam11@mail.ru>
2021-01-31 14:43:26 +0400

    camera_clamp

--- scripts/CameraController.gd
+++ scripts/CameraController.gd
@@ -2,6 +2,7 @@ extends Camera
 
 var _state = Enums.ControllState.None
 
+onready var area : CollisionShape = get_node("/root/TestScene/CameraArea")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,6 +10,15 @@ func _ready():
 func _unhandled_input(event):
 	if event is InputEventScreenDrag:
 		self.translation -= Vector3(event.relative.x, 0, event.relative.y) * 0.1
+		
+		self.translation.x = clamp(self.translation.x,
+			area.global_transform.origin.x - area.shape.extents.x / 2,
+			area.global_transform.origin.x + area.shape.extents.x / 2 )
+		
+		self.translation.z = clamp(self.translation.z,
+			area.global_transform.origin.z - area.shape.extents.z / 2,
+			area.global_transform.origin.z + area.shape.extents.z / 2 )
+		
 	#elif event is InputEventScreenTouch:
 	#	print(event.as_text())
 		
