7b34fa475567559fbf1546771259c53400a86e1d
dronsteam11 <dronsteam11@mail.ru>
2021-01-31 10:42:00 +0400

    err

--- scripts/Bullet.gd
+++ scripts/Bullet.gd
@@ -6,7 +6,8 @@ export var speed : float = 10.0
 
 var _damage : float = 0
 var _target : Enemy = null
-var _b = false
+var _last_pos : Vector3 = Vector3.ZERO
+
 func set_target(target : Enemy, damage : float) -> void:
 	_target = target
 	_damage = damage
@@ -14,15 +15,17 @@ func set_target(target : Enemy, damage : float) -> void:
 
 func _move_to_target(delta : float) -> void:
 	
-	var dir : Vector3 = _target.global_transform.origin - global_transform.origin
+	if !is_inside_tree(): return
+	
+	var dir : Vector3 = _last_pos - global_transform.origin
 	var distanceThisFrame : float = speed * delta
 
 	if dir.length() <= distanceThisFrame:
 		_hit_target()
 		return
-
+	
 	global_translate(dir.normalized() * distanceThisFrame)
-	look_at(_target.global_transform.origin, Vector3.UP)
+	look_at(_last_pos, Vector3.UP)
 
 func _hit_target():
 	if _target == null:
@@ -35,9 +38,10 @@ func _physics_process(delta):
 	if _target == null:
 		_destroy()
 		return
+	
+	if _target.is_inside_tree():
+		_last_pos = _target.global_transform.origin
 	_move_to_target(delta)
 
 func _destroy() -> void:
-	_b = true
 	queue_free()
-	#call_deferred("queue_free")
